a8ef1888f776baea256d4cca4c3edd7187d9a436
csawai <csawai@gmail.com>
2022-02-03 17:19:42 +0530

    added the routes, json handling and dummy data to check if the api is wroking

--- main.go
+++ main.go
@@ -1,31 +1,46 @@
 package main
 
 import (
-	
 	"encoding/json"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
-	"github.com/gorilla/mux"
+	"strconv"
 )
 
 type Beer struct {
-
-	ID string `json:"id"`
+	ID          string `json:"id"`
 	ImageNumber string `json:"ImageNumber"`
-	Name string `json:"Name"`
-	BeerType string `json:"BeerType"`
+	Name        string `json:"Name"`
+	BeerType    string `json:"BeerType"`
 }
 
 var beers []Beer
 
 func getBeers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
+	json.NewEncoder(w).Encode(beers)
 
+	
 }
 func getBeer(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range beers {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+		}
+	}
 
 }
 
 func createBeer(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
+	var newBeer Beer
+	json.NewDecoder(r.Body).Decode(&newBeer)
+	newBeer.ID = strconv.Itoa(len(beers) + 1)
+	beers = append(beers, newBeer)
+	json.NewEncoder(w).Encode(newBeer)
 
 }
 
@@ -37,8 +52,8 @@ func deleteBeer(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func main () {
-
+func main() {
+	beers = append(beers, Beer{ID: "1", ImageNumber: "hello", Name: " beer", BeerType: "ALe"})
 	router := mux.NewRouter()
 
 	//handlers
@@ -50,7 +65,5 @@ func main () {
 
 	http.ListenAndServe(":4003", router)
 	log.Fatal(http.ListenAndServe(":4003", router))
-
-
 
 }
